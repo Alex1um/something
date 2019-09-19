c42710e6b4d44ec5c9a4d09025ac748a4ea4c90b
assa <ye4h@protonmail.com>
2019-09-19 23:04:14 +0700

    *delete comment *Qt ready

--- trigonQt.py
+++ trigonQt.py
@@ -48,28 +48,38 @@ class Moover(Thread):
         self.time = time
         self.skelet = skelet
         self.main = main
+        self.mi, self.ma = float('inf'), -float('inf')
 
     def run(self):
         while True:
-            v = self.getvis()
+            v = self.get_visible_all()
             for i in range(len(self.objs)):
                 self.objs[i].move()
                 if not self.skelet:
-                    if v[i] and v[i-1] and (self.objs[i].k != self.objs[i - 1].k or self.objs[i - 1].k == self.objs[i].k and self.objs[i].k == 1):
+                    if v[i] and v[i - 1] and (self.objs[i].k != self.objs[i - 1].k or self.objs[i - 1].k == self.objs[i].k and self.objs[i].k == 1):
                         self.main.pyro.append((self.objs[i].point0, self.objs[i].point1, self.objs[i-1].point1))
             sleep(self.time)
             self.main.update()
 
-    def getvis(self):
+    def get_visible_all(self):
         v = []
         ko = 30
-        mi, ma = min(self.objs, key=lambda x: x.point1.x()).point1.x(), max(self.objs, key=lambda x: x.point1.x()).point1.x()
+        self.mi, self.ma = min(self.objs, key=lambda x: x.point1.x()).point1.x(), max(self.objs, key=lambda x: x.point1.x()).point1.x()
         for i in self.objs:
-            if mi + ko < i.point1.x() < ma - ko and i.k == -1 and i.point1.x() != mi and i.point1.x() != ma:
+            x = i.point1.x()
+            if (self.mi + ko <= x < self.ma - ko) and i.k == -1:
                 v.append(False)
             else:
                 v.append(True)
         return v
+
+    def get_visible(self, obj, ko=30):
+        x = obj.point1.x()
+        self.ma, self.mi = max(self.objs, key=lambda x: x.point1.x()).point1.x(), min(self.objs, key=lambda x: x.point1.x()).point1.x()
+        if self.mi + ko < x < self.ma - ko and obj.k == -1:
+            return False
+        else:
+            return True
 
 
 def get_points(foo, unfoo, yp=5, speed=1):
@@ -98,26 +108,25 @@ class Mooving_Pyramid(QMainWindow):
 
     def initUI(self):
         self.setGeometry(0, 0, 1000, 1000)
-
-        self.canvas = QLabel(self)
-        self.canvas.setGeometry(0, 0, 200, 200)
+        self.pbrushes = ((QBrush(Qt.white, Qt.SolidPattern),
+                         QPen(Qt.white, 0)),
+                        (QBrush(Qt.yellow, Qt.SolidPattern),
+                         QPen(Qt.black, 2, Qt.SolidLine)))
+        self.painter = QPainter(self)
 
         self.show()
 
     def paintEvent(self, event):
-        a = QPainter(self)
-        a.setBrush(QBrush(Qt.white, Qt.SolidPattern))
-        a.setPen(QPen(Qt.white, 0))
-        a.begin(self)
-        a.drawRect(0, 0, self.width(), self.height())
-        a.end()
-        qp = QPainter(self)
-        qp.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
-        qp.setPen(QPen(Qt.black, 2, Qt.SolidLine))
-        qp.begin(self)
+        # self.painter = QPainter(self)
+        self.painter.setBrush(self.pbrushes[0][0])
+        self.painter.setPen(self.pbrushes[0][1])
+        self.painter.begin(self)
+        self.painter.drawRect(0, 0, self.width(), self.height())
+        self.painter.setBrush(self.pbrushes[1][0])
+        self.painter.setPen(self.pbrushes[1][1])
         for point in self.pyro:
-            qp.drawPolygon(*point)
-        qp.end()
+            self.painter.drawPolygon(*point)
+        self.painter.end()
         self.pyro = []
 
 
@@ -125,7 +134,7 @@ xs, ys = 500, 100
 yr = 650
 r = 1000
 width = 500
-point = get_points(lambda x: circle(xs, x, r, width), lambda y: uncircle(xs, y, r, width), yp=5, speed=1)
+point = get_points(lambda x: circle(xs, x, r, width), lambda y: uncircle(xs, y, r, width), yp=10, speed=1)
 le = len(point) - 1
 app = QApplication(sys.argv)
 a = Mooving_Pyramid()
@@ -134,5 +143,5 @@ l4 = Line(*params, le // 4, -1)
 l = Line(*params, le * 3 // 4, -1)
 l1 = Line(*params, le // 4)
 l2 = Line(*params, le * 3 // 4)
-b = Moover(a, 0.005, False, l4, l1, l2, l).start()
+Moover(a, 0.005, False, l4, l1, l2, l).start()
 sys.exit(app.exec_())
